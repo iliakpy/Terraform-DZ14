a900ed81b4a7f37b81609b0cb0fb885c702b3952
iliakpy <56565312+iliakpy@users.noreply.github.com>
2020-05-14 15:27:56 +0300

    Update config.tf

--- config.tf
+++ config.tf
@@ -1,17 +1,17 @@
   
 provider "aws" {
- region = "eu-central-1"
+ region = "us-east-2"
 }
 
-resource "aws_s3_bucket" "hw" {
-  bucket = "terrahw"
+resource "aws_s3_bucket" "dz" {
+  bucket = "terraform"
   acl = "public-read"
 }
 resource "aws_instance" "gitmvn" {
- ami = "ami-0e342d72b12109f91"
+ ami = "ami-07c1207a9d40bc3bd"
  instance_type = "t2.micro"
- key_name = "my-key4"
- security_groups = ["launch-wizard-2"]
+ key_name = "my-key1"
+ security_groups = ["EMySecurity1"]
  
  tags = {
     Name = "Git and Maven machine"
@@ -20,7 +20,7 @@ resource "aws_instance" "gitmvn" {
     type        = "ssh"
     user        = "ubuntu"
     agent       = false
-    private_key = "${file("~/.ssh/my-key4.pem")}"
+    private_key = "${file("~/.ssh/my-key1.pem")}"
  }
   
   provisioner "file"{
@@ -32,22 +32,22 @@ resource "aws_instance" "gitmvn" {
  provisioner "remote-exec" {
     inline = [
          "sudo apt update && sudo apt install -y git && sudo apt install -y maven && sudo apt install -y awscli",
-         "git clone https://github.com/PZA-git/boxfuse3.git myapp",
+         "git clone https://github.com/iliak2/boxfuse3.git myapp",
          "cd myapp && mvn package",
          "cd ..",
          "sudo mkdir ~/.aws",
          "sudo mv ~/credentials ~/.aws/credentials",         
-         "aws s3 cp ~/myapp/target/hello-1.0.war s3://terrahw --acl public-read",
+         "aws s3 cp ~/myapp/target/hello-1.0.war s3://terraform --acl public-read",
          ]
  }
 }
 
 
 resource "aws_instance" "tomcat8" {
- ami = "ami-0e342d72b12109f91"
+ ami = "ami-07c1207a9d40bc3bd"
  instance_type = "t2.micro"
- key_name = "my-key4"
- security_groups = ["launch-wizard-2"]
+ key_name = "my-key1"
+ security_groups = ["EMySecurity1"]
  tags = {
     Name = "Tomcat8 machine"
   }
@@ -55,7 +55,7 @@ resource "aws_instance" "tomcat8" {
     type        = "ssh"
     user        = "ubuntu"
     agent       = false
-    private_key = "${file("~/.ssh/my-key4.pem")}"
+    private_key = "${file("~/.ssh/my-key1.pem")}"
   } 
  
  provisioner "remote-exec" {
@@ -63,7 +63,7 @@ resource "aws_instance" "tomcat8" {
          "sudo apt update",
          "sudo apt install -y tomcat8",
          "sudo sleep 1m",
-         "sudo wget -P /var/lib/tomcat8/webapps/ https://terrahw.s3.eu-central-1.amazonaws.com/hello-1.0.war",
+         "sudo wget -P /var/lib/tomcat8/webapps/ https://terraform.s3.eu-central-1.amazonaws.com/hello-1.0.war",
     ]
  }
 }
